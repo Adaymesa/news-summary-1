16892f380a27edca3ec5377c8adc04be7d4754f9
Omar Alvarez <omajul2@hotmail.com>
2016-06-04 15:36:53 +0200

    Use HTTPS in urls

--- app/js/services/SummaryService.js
+++ app/js/services/SummaryService.js
@@ -1,13 +1,13 @@
 newsSummaryApp.service("SummaryService", ["SummaryFactory", "$http", function(SummaryFactory, $http) {
 
   this.getAll = function() {
-    var url = "http://content.guardianapis.com/search?api-key=fb91fb66-f35b-4937-a480-b2b2baea1474&show-fields=all";
+    var url = "https://content.guardianapis.com/search?api-key=fb91fb66-f35b-4937-a480-b2b2baea1474&show-fields=all";
     return $http.get(url).then(_iterateThrough);
   };
 
   function _iterateThrough (response) {
     var articles = [];
-    var summaryUrl = "http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=";
+    var summaryUrl = "https://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=";
     var i = 1;
     response.data.response.results.forEach(function(object) {
       $http.get(summaryUrl + object.webUrl).success(function(response){
